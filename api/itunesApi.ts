f0acaddf39e1ab04e4e3248d6a0c861f952bd1fc
“ravidu-mitrai” <263281074+ravidu-mitrai@users.noreply.github.com>
2026-03-11 13:24:38 +0530

    feat: Solve offline mode  request failed issue

--- api/itunesApi.ts
+++ api/itunesApi.ts
@@ -1,27 +1,31 @@
-import axiosClient from "./axiosClient";
 import { Track } from "../types";
+import axiosClient from "./axiosClient";
 
-export const fetchTracks =  async (searchTerm: string): Promise<Track[]> => {
-    try {
-        const response = await axiosClient.get('/search', {
-            params: {
-                term: searchTerm,
-                media: 'music',
-                entity: 'song',
-                limit: 20,
-            },
-        });
+export const fetchTracks = async (searchTerm: string): Promise<Track[]> => {
+  try {
+    const response = await axiosClient.get("/search", {
+      params: {
+        term: searchTerm,
+        media: "music",
+        entity: "song",
+        limit: 20,
+      },
+    });
 
-        return response.data.results.map((item: any) => ({
-            trackId: item.trackId,
-            trackName: item.trackName,
-            artistName: item.artistName,
-            collectionName: item.collectionName || 'Unknown Album',
-            artworkUrl100: item.artworkUrl100,
-            previewUrl: item.previewUrl,
-        }));
-    } catch (error) {
-        console.error("Error fetching tracks from iTunes:", error);
-        return [];
+    return response.data.results.map((item: any) => ({
+      trackId: item.trackId,
+      trackName: item.trackName,
+      artistName: item.artistName,
+      collectionName: item.collectionName || "Unknown Album",
+      artworkUrl100: item.artworkUrl100,
+      previewUrl: item.previewUrl,
+    }));
+  } catch (error: any) {
+    // if the request failed because there is no connection already show an offline banner.
+    const msg = error?.message || "";
+    if (msg !== "Network Error") {
+      console.error("Error fetching tracks from iTunes:", error);
     }
+    return [];
+  }
 };
